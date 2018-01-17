76987e1821fc99bf8c2bef38961d8850ff22e710
chenwei <596808653@qq.com>
2018-01-17 21:19:11 +0800

    gulpfile.js文件

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+//引包
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+//建任务
+gulp.task("copy-html",function(){
+	gulp.src("*.html")
+		.pipe(gulp.dest("D:\\phpStudy\\WWW\\GM"));
+});
+gulp.task("copy-img",function(){
+	gulp.src("img/**/*")
+		.pipe(gulp.dest("D:\\phpStudy\\WWW\\GM\\img"));
+});
+gulp.task("sassfile",function(){
+	gulp.src("sass/**/*")
+		.pipe(sass())
+		.pipe(gulp.dest("D:\\phpStudy\\WWW\\GM\\css"));
+});
+gulp.task("sassfilelocal",function(){
+	gulp.src("sass/**/*")
+		.pipe(sass())
+		.pipe(gulp.dest("css"));
+});
+//监听
+gulp.task("watchall",function(){
+	gulp.watch("*.html",["copy-html"]);
+	gulp.watch("img/**/*",["copy-img"]);
+	gulp.watch("sass/**/*",["sassfile"]);
+	gulp.watch("sass/**/*",["sassfilelocal"]);
+});
